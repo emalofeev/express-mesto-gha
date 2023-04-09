60bc43af0984bc644df2655e0be573bd360d1b56
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-04-09 21:27:10 +0300

    dev: add celebrate to app

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post(
 );
 app.post(
   "/signup",
-  {
+  celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
@@ -34,7 +34,7 @@ app.post(
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
-  },
+  }),
   createUser,
 );
 
