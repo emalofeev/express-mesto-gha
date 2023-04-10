eaca80c72679a33195b88de01db31c53022732e8
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-04-11 00:36:19 +0300

    fix bugs after autotest

--- models/user.js
+++ models/user.js
@@ -6,25 +6,25 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      default: "Жак-Ив Кусто",
       minlength: 2,
       maxlength: 30,
+      default: "Жак-Ив Кусто",
     },
     about: {
       type: String,
-      default: "Исследователь",
       minlength: 2,
       maxlength: 30,
+      default: "Исследователь",
     },
     avatar: {
       type: String,
-      default:
-        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
       validate: {
         validator: (url) => validator.isURL(url),
         message:
           "URL указан в неверном формате или содержит недопустимые символы",
       },
+      default:
+        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
     },
     email: {
       type: String,
@@ -42,7 +42,7 @@ const userSchema = new mongoose.Schema(
       select: false,
     },
   },
-  { versionKey: false },
+  { versionKey: false }
 );
 
 userSchema.statics.findUserByCredentials = function (email, password) {
