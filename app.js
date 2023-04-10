d53569f288862e2a2214aea9b09de64a616f3f9b
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-04-11 01:02:42 +0300

    fix bugs after autotest

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post(
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().pattern(
-        /(https?:\/\/)(w{3}\.)([-._~:/?#][@!$&'()*+,;=])/
+        /(https?:\/\/)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/
       ),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
